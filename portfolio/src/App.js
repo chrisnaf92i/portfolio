96815a2f662257b16685a2c8787d1a49d70288a2
Christopher Nafrere <nafrere.christopher@gmail.com>
2022-04-01 16:14:38 +0200

    création du header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <header class="header">
+        <h1> Christopher Nafrere </h1>
 
+        <nav class="header__nav">
+          <Link to="/"> <h2> Acceuil </h2> </Link>
+          <Link to="/qui-suis-je"> <h2> Qui suis-je ? </h2> </Link>
+          <Link to="/projet"> <h2> Projet </h2> </Link>
+          <Link to="/contact"> <h2 class="header__nav__btn-contact btn"> Contact </h2> </Link>
+        </nav>
+      </header>
     </div>
   );
 }
