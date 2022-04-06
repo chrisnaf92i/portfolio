c412dda254887959ca48b7feba88cb0c6a0fe5f0
Christopher Nafrere <nafrere.christopher@gmail.com>
2022-04-06 19:11:34 +0200

    Ajout du system de contact et de mailing (back-end & front-end)

--- portfolio/src/page/Contact.js
+++ portfolio/src/page/Contact.js
@@ -38,7 +38,7 @@ export default function Contact() {
       });
 
 
-    const response = await fetch("http://localhost:8000/api/message", {method:"post", headers: {"Content-Type":"application/json"}, body:JSON.stringify(contactData)})
+    const response = await fetch("/api/message", {method:"post", headers: {"Content-Type":"application/json"}, body:JSON.stringify(contactData)})
       .then(() => {
         alert("Message Bien envoyé")
       })
